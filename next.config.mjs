505b29a9b89f75954f5a1bf5af1335c381fe2d38
kaii232 <lim.xinkai02@gmail.com>
2025-02-28 11:19:02 +0800

    Set up GitHub Actions for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   basePath: "/bazi-five-elements",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Fixes image loading issues on GitHub Pages
+  },
 };
 
 export default nextConfig;
